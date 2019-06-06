c5aebc16000bafe6c612422b9360e8fd0420bace
Ashwin Bordoloi <ashwin1014@yahoo.co.in>
2019-06-06 16:54:55 +0530

    added withRouter to access history

--- src/App.js
+++ src/App.js
@@ -3,23 +3,57 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
-import { Route, Switch  } from 'react-router-dom';
+import { firebaseApp } from './config';
+import { Route, Switch } from 'react-router-dom';
 import { withRouter } from "react-router";
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { logUser } from './actions/index';
 import 'materialize-css/dist/css/materialize.min.css';
 
-const App = () => {
-  return (
-      <>
-        <Navbar/>
-        <div className="App">
-          <Switch path="/">
-            <Route exact path="/" component={Home}/>
-            <Route path="/signin" component={SignIn}/>
-            <Route path="/signup" component={SignUp}/>
-          </Switch>
-        </div>
-      </>
-  )
+class App extends React.Component {
+
+  componentDidMount() {
+    //  console.log(this.props)
+
+    firebaseApp.auth().onAuthStateChanged(user => {
+      if(user) {
+             const { email, phoneNumber } = user;
+             let signedInType = email ? email:phoneNumber; 
+            //  console.log(signedInType);
+             this.props.logUser(signedInType);
+             console.log('User signed in')
+             this.props.history.push('/')          
+      } else {
+          console.log('User signed out')
+          this.props.history.push('/signin')
+      }
+    })
+  }
+
+    render() {
+      return (
+        <>
+          <Navbar/>
+          <div className="App">
+            <Switch path="/">
+              <Route exact path="/" component={Home}/>
+              <Route path="/signin" component={SignIn}/>
+              <Route path="/signup" component={SignUp}/>
+            </Switch>
+          </div>
+        </>
+    )
+    }
 }
 
-export default withRouter(App);
+const mapStateToProps = (state) => {
+  const { user } = state;
+  return {
+    user
+  }
+}
+
+const combineApp = compose(withRouter, connect(mapStateToProps, { logUser }));
+
+export default combineApp(App)
--- src/index.js
+++ src/index.js
@@ -1,29 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { firebaseApp } from './config';
+// import { firebaseApp } from './config';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/rootReducer';
-import { BrowserRouter, Redirect  } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { logUser } from './actions';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer);
-
-firebaseApp.auth().onAuthStateChanged(user => {
-    if(user) {
-           const { email, phoneNumber } = user;
-           let signedInType = email ? email:phoneNumber; 
-           console.log(signedInType);
-           store.dispatch(logUser(signedInType));
-           console.log('User signed in')
-        return <Redirect to='/'/>
-    } else {
-        console.log('User signed out')
-        return <Redirect to='/signin'/>
-    }
-  }) 
+const store = createStore(rootReducer); 
 
 
 ReactDOM.render(    
